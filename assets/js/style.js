e0dc8ecf76b2915338155b3cbd8ba7e72517e1e3
JessGiannini <jessgiannini@gmail.com>
2021-08-11 21:19:45 -0700

    updating variables

--- assets/js/style.js
+++ assets/js/style.js
@@ -4,25 +4,25 @@
 // api for 5 day forecast api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
 // UV index found here https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
 
-var url = "https://api.openweathermap.org";
+// api source - "https://api.openweathermap.org";
 const apiKey = "012954b21ea1183b476f24736bd1bfdd";
-var searchHistory = [];
+const searchHistory = [];
 
 // DOM elements
 
-var searchForm = document.querySelector("#search-form");
-var searchInput = document.querySelector("#search-input");
-var todayContainer = document.querySelector("#display-current");
-var forecastContainer = document.querySelector("#forecast");
-var searchHistoryContainer = document.querySelector("#history");
+const searchForm = document.querySelector("#search-form");
+const searchInput = document.querySelector("#search-input");
+const todayContainer = document.querySelector("#display-current");
+const forecastContainer = document.querySelector("#forecast");
+const searchHistoryContainer = document.querySelector("#history");
 
 //Forecast url
 
-var todaysForecastUrl =
+const todaysForecastUrl =
   "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
-var fiveDayForecastUrl =
+const fiveDayForecastUrl =
   "api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}";
-var uvIndexUrl =
+const uvIndexUrl =
   "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}";
 
 //FUNCTIONS
