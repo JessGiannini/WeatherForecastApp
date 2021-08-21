e3d2062c20289a760cf4a830d32f9e238c90313b
JessGiannini <jessgiannini@gmail.com>
2021-08-21 16:03:15 -0700

    setting the data to the page

--- README.md
+++ README.md
@@ -1,31 +1,63 @@
-# WeatherForecastApp
+## Third Party APIs
+[Back to Course Content](../../README.md)
 
-The client asked for me to create dynamic web application quiz. They wanted a timer and 5 true and false questions. They also wanted a high score feature.
+-----
+### :book: **[Student Resources](student-resources/README.md)**
 
-The code for this particular project was challenging. This section in particular was difficult to thing through but rewarding when it worked.
+> `Student Resources` are a great way to explore examples, vocabulary, and resourcesful links related to this section of the class.
 
-![JavaScript](/img/ScreenShotQuizMe.png)
+> Great for obtaining a better **conceptual understanding** of the course material. 
 
-## Built With
+------
+### :dart: **[Code Drills](code-drills/README.md)**
 
-- [HTML](https://developer.mozilla.org/en-US/docs/Web/HTML)
-- [CSS](https://developer.mozilla.org/en-US/docs/Web/CSS)
-- [Javascript](https://developer.mozilla.org/en-US/docs/Web/JavaScript)
+> `Code Drills` are a great way to get hands on keyboard, writing syntax, doing work, and encountering problem sets. 
 
-## Deployed Link
+> Great for obtaining a better **practical understanding** of the course material. 
 
-- [See Live Site](https://github.com/JessGiannini/QuizMe)
+-----
+### :pencil: **[Homework](homework/README.md)**
 
-## Authors
+- Third-Party APIs Homework: Work Day Scheduler
+[Homework Instructions Link](homework/README.md)
+    * Create a simple calendar application that allows the user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
 
-**Jessica Giannini**
+-----
 
-* [Link to Portfolio Site](https://jessgiannini.github.io/WebDeveloperPortfolio/)
-* [Link to Github](https://github.com/jessgiannini)
-* [Link to LinkedIn](https://www.linkedin.com/in/jessica-aletta-giannini-155b1310/)
+### :fire: **[WarmUp Challenge](warm-up-challenge)**
 
-## Acknowledgments
+> Get your hands dirty with a daily coding challenge
 
-- Thank you to my amazing classmates who help me navigate the ins and outs of coding.
-- Thanks to all the YouTube stars who helped me see the many ways to pet a cat.
-- Thanks to my friend Kelsey for inspiring me to take this course.
+-----
+
+### :computer: Activity
+
+|  Activities |  Description |
+|:--	|:--
+|[01-Ins_jQuery-Elements](activities/01-Ins_jQuery-Elements)| jQuery Elements - Introduction |
+|[02-Stu_jQuery-Elements](activities/02-Stu_jQuery-Elements)| jQuery elements - Activity |
+|[03-Ins_Click-Events](activities/03-Ins_Click-Events)| Click events - Introduction |
+|[04-Stu_Click-Events](activities/04-Stu_Click-Events)| Click events - Activity |
+|[05-Ins_Form-Elements](activities/05-Ins_Form-Elements)| Form elements - jquery - Introduction |
+|[06-Stu_Form-Elements](activities/06-Stu_Form-Elements)| Form elements - jQuery - Activity |
+|[07-Ins_Dom-Traversal](activities/07-Ins_Dom-Traversal)| Dom Traversal - jQuery - Introduction |
+|[08-Stu_Dom-Traversal](activities/08-Stu_Dom-Traversal)| Dom Traversal - jQuery - Activity |
+|[09-Ins_Event-Delegation](activities/09-Ins_Event-Delegation)| Event Delegation - Introduction |
+|[10-Stu_Event-Delegation](activities/10-Stu_Event-Delegation)| Event Delegation - Activity |
+|[11-Ins_Bootstrap-Components](activities/11-Ins_Bootstrap-Components)| Introduction to Bootstrap components |
+|[12-Stu_Bootstrap-Components](activities/12-Stu_Bootstrap-Components)| Bootstrap components activity |
+|[13-Ins_Bootstrap-Layout](activities/13-Ins_Bootstrap-Layout)| Introduction to Bootstrap Layout |
+|[14-Stu_Bootstrap-Layout](activities/14-Stu_Bootstrap-Layout)| Bootstrap Layout activity |
+|[15-Ins_Bootstrap-Utilities](activities/15-Ins_Bootstrap-Utilities)| Introduction to Bootstrap Utilities
+|[16-Stu_Bootstrap-Utilities](activities/16-Stu_Bootstrap-Utilities)| Bootstrap Utilities activity
+|[17-Ins_Bootstrap-Extend](activities/17-Ins_Bootstrap-Extend)| Bootstrap |
+|[18-Stu_Bootstrap-Extend](activities/18-Stu_Bootstrap-Extend)| Bootstrap |
+|[19-Ins_Google-Fonts](activities/19-Ins_Google-Fonts)| Introduction to Google Fonts |
+|[20-Stu_Google-Fonts](activities/20-Stu_Google-Fonts)| Google Fonts activity |
+|[21-Ins_jQuery-UI-Widgets](activities/21-Ins_jQuery-UI-Widgets)| Introduction to jQuery UI Widgets |
+|[22-Stu_jQuery-UI-Widgets](activities/22-Stu_jQuery-UI-Widgets)| jQuery UI Widgets activity |
+|[23-Ins_jQuery-UI-Interactions](activities/23-Ins_jQuery-UI-Interactions)| Introduction to jquery UI Interactions |
+|[24-Stu_jQuery-UI-Interactions](activities/24-Stu_jQuery-UI-Interactions)| jquery UI Interactions activity |
+|[25-Ins_Moment-Format](activities/25-Ins_Moment-Format)| Introduction to MomentJs |
+|[26-Stu_Moment-Format](activities/26-Stu_Moment-Format)| MomentJs activity |
+|[27-Evr_Git-Revert](activities/27-Evr_Git-Revert)| Git Revert |
--- assets/js/style.js
+++ assets/js/style.js
@@ -5,43 +5,253 @@
 // UV index found here https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
 
 // api source - "https://api.openweathermap.org";
-const apiKey = "012954b21ea1183b476f24736bd1bfdd";
-const searchHistory = [];
+const apiKey = "f068ed2af579c2ab72a944c7c280f68e";
+let latitude;
+let longitude;
+//Forecast url
+// const todaysForecastUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+// const fiveDayForecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`;
+// const oneCallUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=&appid=${apiKey}`;
+
+
+//0. dom selector for where it gets put <div> unique ID
+const todayContainer = $("#display-current");
+const forecastContainer = $("#forecast");
+const searchHistoryContainer = $("#history");
+
+function findLocationWeatherData(event) {
+  event.preventDefault();
+  console.log("this is the findlocationweatherdata");
+  const searchInput = $("#search-input").val();
+  console.log(searchInput);
+  // getWeather()
+  findCurrentWeatherData(searchInput)
+}
+
+//1. dom selector to target the button that is pressed to add the city
+//a. make html with unique id for a button
+//b. add event listener to the button listening for click of element when clicked execute a function findLocationWeatherData()
+// const searchInput = $("#search-input");
+const searchButton = $("#search-button");
+console.log(searchButton);
+searchButton.on("click", findLocationWeatherData);
+
+//2. from within the findLocationWeatherData()
+//a. grab hold of user input value assign it to variable cityName
+//b. execute/call findCurrentWeatherData(cityName)
+//3. create findCurrentWeatherData() takes in cityName as argument to be added to api
+//a. fetch the data from the api end point using cityName as the parameter and select the data needed for our display (temperature, cloud, emoji, etc.)
+
+function findCurrentWeatherData(cityName) {
+  console.log("this is city name", cityName);
+  let requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=${apiKey}`;
+  $.ajax({
+    url: requestUrl,
+    method: "GET",
+  }).then(function (response) {
+    console.log("Single Day Forecast", response);
+    latitude = response.coord.lat;
+    longitude = response.coord.lon;
+    console.log("lat lon", latitude, longitude);
+    displayCurrentWeather(response);
+
+    // displayDayWeather(response);
+    // const customWeatherDataObj = {};
+    // customWeatherDataObj.currentWeather = {};
+    // customWeatherDataObj.currentWeather.weather = response.weather;
+    // customWeatherDataObj.currentWeather.wind.deg = response.wind.deg;
+    // customWeatherDataObj.currentWeather.wind.speed = response.speed;
+
+    findFiveDayForecastWeatherData(cityName)
+
+  });
+}
+
+//4. create findFiveDayForecastWeatherData() takes in cityName as argument to be added to api end point
+
+function findFiveDayForecastWeatherData(cityName) {
+  let fiveDayUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=imperial&appid=${apiKey}`;
+  $.ajax({
+    url: fiveDayUrl,
+    method: "GET",
+  }).then(function (response) {
+    console.log("Five Day Forecast", response);
+    findUvWeatherData(latitude,longitude)
+    // weatherobj.fiveDay = {};
+    //weatherobj.fiveDay = response.
+  })
+}
+
+//https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}
+
+//5. create findUVForecastWeatherData() takes in lat and lon as argument to be added to argument
+
+function setUVIcolor(uvi) {
+  if (uvi >= 10) {
+    $(".uvi").css('color', 'red');
+  } else if (uvi >= 5) {
+    $(".uvi").css('color', 'yellow');
+  } else { 
+    $(".uvi").css('color', 'green');
+  }
+}
+
+function findUvWeatherData(lat, lon) {
+  let UvUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=&appid=${apiKey}`;
+  $.ajax({
+    url: UvUrl,
+    method: "GET",
+  }).then(function (response) {
+    console.log("UV Index", response);
+    $(".uvi").text(`UV INDEX: ${response.current.uvi}`)
+    console.log("UVI============", response.current.uvi);
+    setUVIcolor(response.current.uvi);
+  });
+}
+
+function displayCurrentWeather(res) {
+  console.log("City Name",res.name)
+  $("#current-location").text(res.name);
+  $("#icon").attr({
+    "src": `https://openweathermap.org/img/wn/${res.weather[0].icon}@2x.png`,
+    "width": "80px" // Should make a little bigger
+  })
+  $("#current-temp").text(`TEMP: ${res.main.temp}`);
+  $("#current-wind").text(`WIND: ${res.wind.speed}`);
+  $("#current-hum").text(`HUMIDITY: ${res.main.humidity}`);
+  $("#current-date").text(moment().format("MMM Do YYYY"));
+  // $("#current-uv").text(res.name);
+  // name, date, temp, windspeed, icon, humidity
+}
+
+//https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=&appid=${apiKey}
+
+// display weather function
+
+// function displayWeatherData(infoObj) {
+
+//   // todayContainer;
+//   // forecastContainer;
+//   // searchHistoryContainer;
+// }
+
+// function getWeather(city) {
+//   findCurrentWeatherData();
+//   findFiveDayForecastWeatherData();
+//   findUvWeatherData()
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//"012954b21ea1183b476f24736bd1bfdd";
+// const searchHistory = [];
+// let lat;
+// let lon;
 
 // DOM elements
 
-const searchForm = document.querySelector("#search-form");
-const searchInput = document.querySelector("#search-input");
-const todayContainer = document.querySelector("#display-current");
-const forecastContainer = document.querySelector("#forecast");
-const searchHistoryContainer = document.querySelector("#history");
+// const searchForm = $("#search-form");
+// const searchInput = $("#search-input");
+// const searchButton = $("#search-button");
+// const todayContainer = $("#display-current");
+// const forecastContainer = $("#forecast");
+// const searchHistoryContainer = $("#history");
 
-//Forecast url
+//event listeners
 
-const todaysForecastUrl =
-  "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
-const fiveDayForecastUrl =
-  "api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}";
-const uvIndexUrl =
-  "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}";
+// searchButton.click(async function (e) {
+//   e.preventDefault();
+//   // searchInput.val();
+
+//   var cityName = searchInput.value.trim();
+
+//   if (username) {
+//     getUserRepos(username);
+
+//     repoContainerEl.textContent = "";
+//     nameInputEl.value = "";
+//   } else {
+//     alert("Please enter a GitHub username");
+//   }
+//   console.log("this is the search", searchInput.val());
+//   fetch(
+//     `https://api.openweathermap.org/data/2.5/forecast?q=${searchInput.val()}=${apiKey}`
+//   )
+//     .then((response) => response.json())
+//     .then((data) => {
+//       console.log(data)((lat = data.lat)),
+//         (lon = data.lon),
+//         console.log(lat, lon);
+//     })
+//     .then(() => {
+//       fetch(
+//         `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=&appid=${apiKey}`
+//       )
+//         .then((response) => response.json())
+//         .then((data) => {
+//           // const oneCallData = {}
+//           console.log(data);
+//         });
+//     });
+// });
+
 
 //FUNCTIONS
 
+//searchCity();
 //displayHistory();
 //updateHistory();
 //clearHistory();
+
 //fetchCurrent();
+
 //fetchForecast();
 //fetchUvIndex();
 
-// fetch(
-//   "https://api.openweathermap.org/data/2.5/forecast?q=London&appid=012954b21ea1183b476f24736bd1bfdd"
-// )
-//   .then((Response) => Response.json())
-//   .then((data) => {
-//     console.log(data);
-//     for (var i = 0; i < 5; i++) {
-//       console.log(data.list[i * 8]);
-//       $data.list[i * 8].main.humidity;
-//     }
-//   });
+// function fetchLatLon() {
+//   fetch(fiveDayForecastUrl)
+//     .then(function (response) {
+//       console.log("response", response);
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log("data", data);
+
+//       (lat = data.city.coord.lat),
+//         (lon = data.city.coord.lon),
+//         console.log(lat, lon);
+//     });
+// }
+
+// searchButton.addEventListener("click", fetchLatLon);
+
+// function fetchOneCall() {
+//   fetch(
+//     `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=&appid=${apiKey}`
+//   )
+//     .then((response) => response.json())
+//     .then((data) => {
+//       // const oneCallData = {}
+//       console.log(data);
+//     });
+// }
+
+// function getWeather() {
+//   fetchLatLon("London").then(() => fetchOneCall(lat, lon));
+// }
+
+// getWeather();
+
+//==============================================================
+
